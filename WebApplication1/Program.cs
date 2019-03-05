8eb7b44fb1626943b9aa97bdd2babb6672208e89
p.maslouski <p.maslouski@sam-solutions.com>
2019-03-05 15:28:24 +0300

    Initial code

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+        //    public static void Main(string[] args)
+        //    {
+        //        Console.Title = "IdentityServer";
+
+        //        var host = new WebHostBuilder()
+        //            .UseKestrel()
+        //            // задаём порт, и адрес на котором Kestrel будет слушать
+        //            .UseUrls("http://localhost:5000")
+        //            // имеет значения для UI логина-логаута 
+        //            .UseContentRoot(Directory.GetCurrentDirectory())
+        //            .UseIISIntegration()
+        //            .UseStartup<Startup>()
+        //            .Build();
+
+        //        host.Run();
+        //    }
+    }
+}
